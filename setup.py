050157e5f55ea53549fcc10c3c44a620080b7d8c
chriss <mail_to_chriss@posteo.net>
2020-10-19 15:48:43 +0200

    version inc

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(here, 'README.md'), 'r') as fh:
 
 setuptools.setup(
     name='starlette-authx',
-    version='0.0.9',
+    version='0.0.10',
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Christoph Hartmann',
